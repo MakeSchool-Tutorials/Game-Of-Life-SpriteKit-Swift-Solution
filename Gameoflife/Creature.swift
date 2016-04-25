4f6fb07e3d59dd9a1bd1f91d3d2605d4af6b27bb
Martin Walsh <martin.walsh@gmail.com>
2016-04-25 16:57:45 +0100

    Initial commit
    Project 1st draft

---
+++ Gameoflife/Creature.swift
@@ -0,0 +1,40 @@
+//
+//  Creature.swift
+//  Gameoflife
+//
+//  Created by Martin Walsh on 24/04/2016.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import SpriteKit
+
+class Creature: SKSpriteNode {
+    
+    /* Character side */
+    var isAlive: Bool = false {
+        didSet {
+            /* Visibility */
+            hidden = !isAlive
+        }
+    }
+    
+    /* Living neighbor counter */
+    var neighborCount = 0
+    
+    init() {
+        /* Initialize with 'bubble' asset */
+        let texture = SKTexture(imageNamed: "bubble")
+        super.init(texture: texture, color: UIColor.clearColor(), size: texture.size())
+        
+        /* Set Z-Position, ensure ontop of grid */
+        zPosition = 1
+        
+        /* Set anchor point to bottom-left */
+        anchorPoint = CGPoint(x: 0, y: 0)
+    }
+    
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
--- Gameoflife/GameScene.swift
+++ Gameoflife/GameScene.swift
@@ -9,37 +9,84 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    /* Game objects */
+    var gridNode: Grid!
+    
+    /* UI Objects */
+    var stepButton: MSButtonNode!
+    var populationLabel: SKLabelNode!
+    var generationLabel: SKLabelNode!
+    var playButton: MSButtonNode!
+    var pauseButton: MSButtonNode!
+    
+    /* Simulation sequence */
+    var simulation: SKAction!
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        /* Connect scene objects */
+        gridNode = childNodeWithName("gridNode") as! Grid
+        
+        /* Connect UI scene objects */
+        stepButton = childNodeWithName("stepButton") as! MSButtonNode
+        populationLabel = childNodeWithName("populationLabel") as! SKLabelNode
+        generationLabel = childNodeWithName("generationLabel") as! SKLabelNode
+        playButton = childNodeWithName("playButton") as! MSButtonNode
+        pauseButton = childNodeWithName("pauseButton") as! MSButtonNode
+        
+        /* Setup testing button selected handler */
+        stepButton.selectedHandler = {
+            self.stepSimulation()
+        }
+        
+        /* Setup play button selected handler */
+        playButton.selectedHandler = {
+            self.paused = false
+        }
+        
+        /* Setup pause button selected handler */
+        pauseButton.selectedHandler = {
+            self.paused = true
+        }
+        
+        /* Create an SKAction based timer, 0.5 second delay */
+        let delay = SKAction.waitForDuration(0.5)
+        
+        /* Call the stepSimulation() method to advance the simulation */
+        let callMethod = SKAction.performSelector("stepSimulation", onTarget: self)
+        
+        /* Create the delay,step cycle */
+        let stepSequence = SKAction.sequence([delay,callMethod])
+        
+        /* Create an infinite simulation loop */
+        simulation = SKAction.repeatActionForever(stepSequence)
+        
+        /* Run simulation action */
+        self.runAction(simulation)
+        
+        /* Default simulation to pause state */
+        self.paused = true
+
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        /* Called when a touch begins */
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func stepSimulation() {
+        /* Step Simulation */
+        
+        /* Run next step in simulation */
+        gridNode.evolve()
+        
+        /* Update UI label objects */
+        populationLabel.text = String(gridNode.population)
+        generationLabel.text = String(gridNode.generation)  
     }
 }
